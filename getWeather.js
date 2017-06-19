18593f4b4d9cf9bf236901088bd1066f96efaf54
FaalVedtuz <jmr_mymsaccnt@outlook.com>
2017-06-20 05:19:01 +0800

    added current weather condition and temp values.

--- getWeather.js
+++ getWeather.js
@@ -23,19 +23,26 @@ function getWeatherFn(la,lo) {
         url: "https://simple-weather.p.mashape.com/weatherdata?lat="+la+"&lng="+lo+"", // The URL to the API.
         type: 'GET',
         //data: {}, // Additional parameters here
-        
+
         dataType: 'json',
         success: function(data) {
-        var weatherDesc = data.query.results.channel.description;
-        var imgIcon = data.query.results.channel.image.url; 
+        var city = data.query.results.channel.location.city;
+        var country = data.query.results.channel.location.country;
+        var region = data.query.results.channel.location.region;
+        var condition = data.query.results.channel.item.condition.text;
+        var temp = data.query.results.channel.item.condition.temp + " " + data.query.results.channel.units.temperature;
+        var imgIcon = data.query.results.channel.image.url;
         var imgWidth = data.query.results.channel.image.width;
         var imgHeigth = data.query.results.channel.image.height;
 
-            console.log(weatherDesc);
-           $("#weatherContainer").html("<h3 class='red-text text-lighten-2 center-align'>"+ weatherDesc +"</h3>");
+        console.log(condition);
+        console.log(temp);
+           $("#weatherContainer").html("<h5 class='red-text text-lighten-2 center-align'>"+ city + ", " + region + ", "+ country + "</h5>");
            $("#weatherIcon").html("<img class='circle' width='"+ imgWidth +"' heigth='"+ imgHeigth +"' src='"+ imgIcon +"'>");
+           $("#currentDayCondition").html("<strong>Weather Condition: </strong>" + condition);
+           $("#currentDayTemp").html("<strong>Weather Temperature: </strong>"+ temp);
            // $("#weatherContainer").html("<h5 class='flow-text blue-text text-darken-4'>- " + dta + "</h5>");
-           
+
         },
         error: function(err) {
             console.log(err);
@@ -44,6 +51,6 @@ function getWeatherFn(la,lo) {
             xhr.setRequestHeader("X-Mashape-Authorization", "li7CFnVytCmshu5vqwd6FMFiEyoXp1Wv4u2jsnnSx2D8MFAHFF"); // Mashape key
         },
 
-        
+
     });
 }
