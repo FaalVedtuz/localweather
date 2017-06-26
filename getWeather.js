7077c0ba81a403826ea1492b9d5fb36849cfa1e5
Jayson Rondina <jmr_mymsaccnt@hotmail.com>
2017-06-27 05:13:45 +0800

    added dyanmically appending cards.

--- getWeather.js
+++ getWeather.js
@@ -27,6 +27,7 @@ function getWeatherFn(la,lo) {
 
         dataType: 'json',
         success: function(data) {
+
         var city = data.query.results.channel.location.city;
         var country = data.query.results.channel.location.country;
         var region = data.query.results.channel.location.region;
@@ -35,16 +36,17 @@ function getWeatherFn(la,lo) {
         var temp = data.query.results.channel.item.condition.temp + "&#176; "+ tempType;
         var imgIcon = getWeatherIcon(condition);
         var currentFetchDate = data.query.results.channel.lastBuildDate;
+        var getDay = data.query.results.channel.item.forecast;
 
-        console.log(condition);
-        console.log(temp);
            $("#weatherContainer").html("<h4 class='red-text text-lighten-2 center-align'>"+ city + ", " + region + ", "+ country + "</h4>");
            $("#weatherIcon").html("<img class='weatherImg responsive-img right' alt='"+ condition +"' src='"+ imgIcon +"'>");
            $("#currentDayCondition").html(condition);
            $("#currentDayTemp").html(temp);
            $("#currentFetchDate").html(currentFetchDate);
-           // $("#weatherContainer").html("<h5 class='flow-text blue-text text-darken-4'>- " + dta + "</h5>");
-
+           for(var i =1; i < 5; i++){
+                $("#cardContainer").append("<div class='col s12 m3 l3 '><div class='card-panel blue lighten-3'><div class='card-content'><span class='card-title white-text flow-text'>"+ 
+                getDay[i].day+"</span><div class='row'><div class='col s6 m6 l6'><img class='weatherCardImg' src='images/cloudy.png' alt='cloudy' /></div><div class='col s6 m6 l6'><p>Sample Content</p></div></div></div></div></div></div>");
+            }
         },
         error: function(err) {
             console.log(err);
@@ -53,18 +55,19 @@ function getWeatherFn(la,lo) {
             xhr.setRequestHeader("X-Mashape-Authorization", "li7CFnVytCmshu5vqwd6FMFiEyoXp1Wv4u2jsnnSx2D8MFAHFF"); // Mashape key
         },
 
-        
-
     });
 
     function getWeatherIcon(w_condition){
-            switch (w_condition){
-                case 'Cloudy':
-                    return 'images/cloud.png';
-                case 'Partly Cloudy':
-                    return'images/partly-cloudy.png';
-                case 'Mostly Sunny':
-                    return'images/partly-cloudy.png';
-            }
+        switch (w_condition){
+            case 'Cloudy':
+                return 'images/cloud.png';
+            case 'Partly Cloudy':
+                return'images/partly-cloudy.png';
+            case 'Mostly Sunny':
+                return'images/partly-cloudy.png';
+            case 'Thunderstorms':
+                return'images/thunderstorm.png';
         }
+    }
+
 }
