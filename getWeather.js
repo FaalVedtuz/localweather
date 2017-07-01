6014630489ad24ff62216ec1110e829adaf0fa8c
Jayson Rondina <jmr_mymsaccnt@hotmail.com>
2017-07-01 17:26:25 +0800

    commit for version 1

--- getWeather.js
+++ getWeather.js
@@ -1,74 +1,116 @@
-$(document).ready(function(){
- getLocationFn();
+$(document).ready(function () {
+    $("#changeTemp").attr('checked', false);
+    getLocationFn();
 });
 
-function getLocationFn(){
-    if(navigator.geolocation){
+function getLocationFn() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getCoordsFn);
-    }else{
+    } else {
         $("#weatherContainer").html("<h3 class='red-text text-darken-2'>Sorry your browser does not support geo location.</h3>");
     }
 }
 
-function getCoordsFn(position){
+function getCoordsFn(position) {
     var lati = position.coords.latitude;
     var longi = position.coords.longitude;
-    getWeatherFn(lati,longi);
+    getWeatherFn(lati, longi);
     console.log(lati + " " + longi);
 }
 
 
-function getWeatherFn(la,lo) {
+function getWeatherFn(la, lo) {
+
+    // init global variables
+    var bgImg = "";
+    var rawTemp = "";
+    var temp = "";
+    // -------------------------//
+
     $.ajax({
 
-        url: "https://simple-weather.p.mashape.com/weatherdata?lat="+la+"&lng="+lo+"", // The URL to the API.
+        url: "https://simple-weather.p.mashape.com/weatherdata?lat=" + la + "&lng=" + lo + "", // The URL to the API.
         type: 'GET',
         //data: {}, // Additional parameters here
 
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
 
-        var city = data.query.results.channel.location.city;
-        var country = data.query.results.channel.location.country;
-        var region = data.query.results.channel.location.region;
-        var condition = data.query.results.channel.item.condition.text;
-        var tempType = data.query.results.channel.units.temperature;
-        var temp = data.query.results.channel.item.condition.temp + "&#176; "+ tempType;
-        var imgIcon = getWeatherIcon(condition);
-        var currentFetchDate = data.query.results.channel.lastBuildDate;
-        var getDay = data.query.results.channel.item.forecast;
+            var city = data.query.results.channel.location.city;
+            var country = data.query.results.channel.location.country;
+            var region = data.query.results.channel.location.region;
+            var condition = data.query.results.channel.item.condition.text;
 
-           $("#weatherContainer").html("<h4 class='red-text text-lighten-2 center-align'>"+ city + ", " + region + ", "+ country + "</h4>");
-           $("#weatherIcon").html("<img class='weatherImg responsive-img right' alt='"+ condition +"' src='"+ imgIcon +"'>");
-           $("#currentDayCondition").html(condition);
-           $("#currentDayTemp").html(temp);
-           $("#currentFetchDate").html(currentFetchDate);
-           for(var i =1; i < 5; i++){
-                $("#cardContainer").append("<div class='col s12 m3 l3 '><div class='card-panel blue lighten-3 z-depth-3'><div class='card-content'><span class='card-title white-text flow-text'>"+ 
-                getDay[i].day+"</span><div class='row'><div class='col s4 m4 14 offset-m3 offset-s3'><img class='weatherCardImg' src='"+ getWeatherIcon(getDay[i].text) +"' alt='" + getDay[i].text +"' /></div></div><div class='row'><div class='col s12 m12 l12'><p class='flow-text center-align'>" + getDay[i].text + "</p></div></div></div></div></div></div>");
+            var imgIcon = getWeatherCondtion(condition);
+            var currentFetchDate = data.query.results.channel.lastBuildDate;
+            var getDay = data.query.results.channel.item.forecast;
+
+            var tempType = data.query.results.channel.units.temperature;
+            rawTemp = data.query.results.channel.item.condition.temp;
+            temp = rawTemp + "&#176; " + tempType;
+            var tempFlag = true;
+
+            $("#changeTemp").on('change', function () {
+                var status = $(this).prop("checked");
+                var tempVal = data.query.results.channel.item.condition.temp;
+                if (tempFlag) {
+                    if (status === true) { // convert to F
+                        rawTemp = (tempVal * (9 / 5)) + 32;
+                        tempFlag = false;
+                    }
+                }
+                switch(status){
+                    case true:
+                        $("#currentDayTemp").html($("refresh-temp").html());
+                        $("#currentDayTemp").html(rawTemp + "&#176; F");
+                        break;
+                    default:
+                        $("#currentDayTemp").html($("refresh-temp").html());
+                        $("#currentDayTemp").html(temp);
+                        break;
+                }
+            });
+
+            $("#weatherContainer").html("<h4 class='center-align'>" + city + ", " + region + ", " + country + "</h4>");
+            $("#weatherIcon").html("<img class='weatherImg responsive-img right' alt='" + condition + "' src='" + imgIcon + "'>");
+            $("#currentDayCondition").html(condition);
+            $("#currentDayTemp").html(temp);
+            $("#currentFetchDate").html(currentFetchDate);
+            $("body").css('background', 'url(' + bgImg + ')');
+            for (var i = 1; i < 5; i++) {
+                $("#cardContainer").append("<div class='col s12 m3 l3 '><div class='card-panel blue lighten-2 z-depth-3'><div class='card-content'><span class='card-title white-text flow-text'>" +
+                    getDay[i].day + "</span><div class='row'><div class='col s8 m8 18 offset-m2 offset-s2'><img class='weatherCardImg' src='" + getWeatherCondtion(getDay[i].text) + "' alt='" + getDay[i].text + "' /></div></div><div class='row'><div class='col s12 m12 l12'><p class='flow-text white-text center-align'>" + getDay[i].text + "</p></div></div></div></div></div></div>");
             }
         },
-        error: function(err) {
+        error: function (err) {
             console.log(err);
         },
-        beforeSend: function(xhr) {
+        beforeSend: function (xhr) {
             xhr.setRequestHeader("X-Mashape-Authorization", "li7CFnVytCmshu5vqwd6FMFiEyoXp1Wv4u2jsnnSx2D8MFAHFF"); // Mashape key
         },
 
     });
 
-    function getWeatherIcon(w_condition){
-        switch (w_condition){
+    function getWeatherCondtion(w_condition) {
+        switch (w_condition) {
             case 'Cloudy':
+                bgImg = 'images/bg-cloudy-day.jpg';
                 return 'images/cloudy.png';
             case 'Partly Cloudy':
-                return'images/partly-cloudy.png';
+                bgImg = 'images/bg-cloud-day.jpg';
+                return 'images/cloudy.png';
             case 'Mostly Sunny':
-                return'images/partly-cloudy.png';
+                bgImg = 'images/bg-clear-day.jpg';
+                return 'images/clear-day.png';
             case 'Thunderstorms':
-                return'images/thunderstorm.png';
+                bgImg = 'images/bg-thunderstorm.jpg';
+                return 'images/thunderstorm.png';
+            case 'Scattered Thunderstorms':
+                bgImg = 'images/bg-thunderstorm.jpg';
+                return 'images/thunderstorm.png';
             case 'Showers':
-                return'images/rain.png';
+                bgImg = 'images/bg-rainy.jpg';
+                return 'images/rain.png';
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 # LOCAL WEATHER
 
+### What does this app do?
+
+This app is a submission to the freeCodeCamp challenge Local Weather. This app detects the usurs location by using html5's geolocation, I did not save the users location for simpler approach and security (if it is an issue), it then get the current weather on the user's location and display it. It can also convert the current temperature to farenheit and vice versa.
+
+#### Frameworks / Tools used
+
+* [http://materializecss.com/](Materialize Framework)
+* [https://code.visualstudio.com/](Visual Studio Code Editor) is really awesome!
+* [https://market.mashape.com](Mashape Marketplace) for the free weather API. You can find more API's here, both free and freemium.
+
